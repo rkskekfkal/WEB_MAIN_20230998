0d626b3849c187cf25bf068afdc0f8025eab7925
rkskekfkal <xxoeyeon7058@sungkyul.ac.kr>
2024-03-30 01:11:22 +0900

    4주차 JS 기능 추가 완료

---
+++ js/Basic_js_test.js
@@ -0,0 +1,31 @@
+
+var jb = 'hi';
+var a = 1;
+var b;
+b = 5;
+
+/*
+여러 줄 주석 : 여러 줄에 걸쳐 주석을 처리합니다.
+*/
+
+if (true) {
+    let c = 'let 접근';
+    var c_1 = 'var 접근';
+}
+//console.log(c); // Error?
+console.log(c_1);
+
+let d = 5;
+//let d = '갑 재할당'; // Error?
+console.log(d);
+
+const e = '상수1 접근';
+//e = 5;
+//const f // Error?
+console.log(e);
+
+document.getElementById("search_message").addEventListener('click', search_message);
+
+function search_message(){
+    alert("검색을 수행합니다!");
+}
