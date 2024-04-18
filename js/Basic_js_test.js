d5b7bb241ead6b4031d0ba7eb58fc5bc4d658e58
rkskekfkal <xxoeyeon7058@sungkyul.ac.kr>
2024-04-18 21:19:59 +0900

    7주차

--- js/Basic_js_test.js
+++ js/Basic_js_test.js
@@ -24,8 +24,22 @@ const e = '상수1 접근';
 //const f // Error?
 console.log(e);
 
+function googleSearch() {
+    const searchTerm = document.getElementById("search_input").value; // 검색어로 설정
+    const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`;
+    // 새 창에서 구글 검색을 수행
+    window.open(googleSearchUrl, "_blank"); // 새로운 창에서 열기.
+    return false;
+}
+    
 document.getElementById("search_message").addEventListener('click', search_message);
 
+/*
 function search_message(){
     alert("검색을 수행합니다!");
 }
+*/
+const search_message = () => {
+    const c = '검색을 수행합니다';
+    alert(c);
+};
