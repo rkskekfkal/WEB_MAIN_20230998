d1f51e6484a6cf1e6e324147ddab264e1c36be52
rkskekfkal <xxoeyeon7058@sungkyul.ac.kr>
2024-04-24 13:26:52 +0900

    8주차

--- js/login.js
+++ js/login.js
@@ -1,4 +1,5 @@
 const check_input = () => {
+
     const loginForm = document.getElementById('login_form');
     const loginBtn = document.getElementById('login_btn');
     const emailInput = document.getElementById('typeEmailX');
@@ -8,6 +9,30 @@ const check_input = () => {
     const emailValue = emailInput.value.trim();
     const passwordValue = passwordInput.value.trim();
 
+
+    const check_xss = (input) => {
+        const DOMPurify = window.DOMPurify;
+        const sanitizedInput = DOMPurify.sanitize(input);
+        if (sanitizedInput !== input) {
+        alert('XSS 공격 가능성이 있는 입력값을 발견했습니다.');
+        return false;
+        }
+        return sanitizedInput;
+    };
+
+    const sanitizedPassword = 
+    check_xss(passwordValue);
+
+    const sanitizedEmail = check_xss(emailValue);
+
+    if (!sanitizedEmail) {
+
+    }
+    if (!sanitizedPassword) {
+
+    return false;
+    }
+        
     if (emailValue === '') {
     alert('이메일을 입력하세요.');
     return false;
@@ -18,9 +43,32 @@ const check_input = () => {
     return false;
     }
     
+    if (emailValue.length < 5) {
+        alert('아이디는 최소 5글자 이상 입력해야 합니다.');
+        return false;
+    }
+    if (passwordValue.length < 12) {
+        alert('비밀번호는 반드시 12글자 이상 입력해야 합니다.');
+        return false;
+    }
+    const hasSpecialChar = passwordValue.match(/[!,@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/) !== null;
+        if (!hasSpecialChar) {
+        alert('패스워드는 특수문자를 1개 이상 포함해야 합니다.');
+        return false;
+    }
+    const hasUpperCase = passwordValue.match(/[A-Z]+/) !== null;
+    const hasLowerCase = passwordValue.match(/[a-z]+/) !== null;
+    if (!hasUpperCase || !hasLowerCase) {
+        alert('패스워드는 대소문자를 1개 이상 포함해야 합니다.');
+        return false;
+    }
+    
+
     console.log('이메일:', emailValue);
     console.log('비밀번호:', passwordValue);
     loginForm.submit();
+
+    
 };
 document.getElementById("login_btn").addEventListener('click', check_input);
     
